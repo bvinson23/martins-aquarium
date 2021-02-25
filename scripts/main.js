d00b495f15ea65fef31dbae7243518e1901086f4
bvinson23 <brandonvinson23@gmail.com>
2021-02-25 15:26:25 -0600

    created scripts directories

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 console.log("you did it");
 
-import { getFish } from './FishData.js'
-import { FishList } from './FishList.js'
+import { getFish } from '../fish/FishData.js'
+import { FishList } from '../fish/FishList.js'
 
 const allTheFish = getFish()
 
